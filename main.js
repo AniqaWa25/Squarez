22c763653fb3807d5f79fb4766644363a001bef1
Aniqa Wahid <Aniqa.wahid25@gmail.com>
2016-01-25 16:39:36 -0500

    Update 2

--- main.js
+++ main.js
@@ -13,14 +13,18 @@ setInterval(function(){
 	var minutes = da.getMinutes();
 
 	if(minutes % 2 === 0){
-		document.body.style.backgroundColor = "#6495ED"
+		document.body.style.backgroundColor = "#A52A2A"
 		console.log(minutes +" EVEN");
 	} else {
-		document.body.style.backgroundColor = "pink"
+		document.body.style.backgroundColor = "#808080"
 		console.log(minutes + " ODD");
 	}
 
 }, 1000)
+
+function gameOn(){
+		document.getElementById("changeUp").innerHTML = "Login!";
+	}
 
 
 $(document).ready(function(){
@@ -36,8 +40,6 @@ $(document).ready(function(){
         $(this).stop().animate({height:'20px'},{queue:false, duration:600})
     });
 
-
-
 //Password & Username Validation Form
 var correctPassword = "12345";
 var correctUser = "Aniqa";
@@ -47,23 +49,27 @@ $("form").submit(function(e){
 	e.preventDefault();
 
 	var firstname = $("#firstname").val()
-	var password = $("#password").val()
-	console.log(firstname + " " + password);
+	// var password = $("#password").val()
+	// console.log(firstname + " " + password);
 
 	if(firstname ===""){
-	$("h1").html("Please fill it out?")
-	} else if (correctPassword === password && correctUser === firstname){
-		console.log(firstname + " YES")
-		$("h1").html("GOOD JOB, DARNIT")
+	$("h1").html("You need to provide a Player name")
+	} else {
+		console.log("should redirect now")
+	} 
 
-	} else{
-		console.log(password + " NOOOOOOOOOOO")
-		$("h1").html("Try Again!");
+	// if (password !== "" && correctUser === firstname){
+	// 	console.log(firstname + " YES")
+	// 	$("h1").html("GOOD JOB, DARNIT")
+
+	// } else{
+	// 	console.log(password + " NOOOOOOOOOOO")
+	// 	$("h1").html("Try Again!");
 
 		$("h1").animate({
 			height: "toggle"
 		}, "slow")
-	}
+	//}
 
 })
 
@@ -81,14 +87,6 @@ $("form").submit(function(e){
 	})
 
 
-//OS Name happening here
-	var OSName="Unknown OS";
-	if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
-	if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
-	if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
-	if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
-
-	console.log('Your OS' + OSName);
 	
 	$("h4").html('Your OS: '+OSName);
 
@@ -100,68 +98,13 @@ $("form").submit(function(e){
 	}
 
 	
-
-//Which Browser happening here
-	var nAgt = navigator.userAgent;
-	var browserName  = navigator.appName;
-	var nameOffset,verOffset,ix;
-
-	// In Opera 15+, the true version is after "OPR/" 
-	if ((verOffset=nAgt.indexOf("OPR/"))!=-1) {
-	 browserName = "Opera";
-	}
-	// In older Opera, the true version is after "Opera" or after "Version"
-	else if ((verOffset=nAgt.indexOf("Opera"))!=-1) {
-	 browserName = "Opera";
-	}
-	// In MSIE, the true version is after "MSIE" in userAgent
-	else if ((verOffset=nAgt.indexOf("MSIE"))!=-1) {
-	 browserName = "Microsoft Internet Explorer";
-	}
-	// In Chrome, the true version is after "Chrome" 
-	else if ((verOffset=nAgt.indexOf("Chrome"))!=-1) {
-	 browserName = "Chrome";
-	}
-	// In Safari, the true version is after "Safari" or after "Version" 
-	else if ((verOffset=nAgt.indexOf("Safari"))!=-1) {
-	 browserName = "Safari";
-	}
-	// In Firefox, the true version is after "Firefox" 
-	else if ((verOffset=nAgt.indexOf("Firefox"))!=-1) {
-	 browserName = "Firefox";
-	}
-	// In most other browsers, "name/version" is at the end of userAgent 
-	else if ( (nameOffset=nAgt.lastIndexOf(' ')+1) < 
-	          (verOffset=nAgt.lastIndexOf('/')) ) 
-	{
-	 browserName = nAgt.substring(nameOffset,verOffset);
-	 if (browserName.toLowerCase()==browserName.toUpperCase()) {
-	  browserName = navigator.appName;
-	 }
-	}
-	
-	$("h5").html('Browser name: '+browserName);
-	if(browserName !== ""){
-		$("h5").animate({
-			fontSize: "30px",
-			opacity: .5,
-		})
-	}
-
-//Size of window happening here
-	var screenW = 640, screenH = 480;
-	if (parseInt(navigator.appVersion)>3) {
-	 screenW = screen.width;
-	 screenH = screen.height;
-	} else if (navigator.appName == "Netscape" 
-	    && parseInt(navigator.appVersion)==3
-	    && navigator.javaEnabled()
-	   ){
-	 var jToolkit = java.awt.Toolkit.getDefaultToolkit();
-	 var jScreenSize = jToolkit.getScreenSize();
-	 screenW = jScreenSize.width;
-	 screenH = jScreenSize.height;
-	}
+	// $("h5").html('Browser name: '+browserName);
+	// if(browserName !== ""){
+	// 	$("h5").animate({
+	// 		fontSize: "30px",
+	// 		opacity: .5,
+	// 	})
+	// }
 
 	$("h6").html("Screen width = "+screenW+"<br>"+"Screen height = "+screenH);
 	
